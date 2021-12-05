ce070ba0b3608339ee47c9407523c87f7c8e8eb7
Youmeng <youmenghin15@gmail.com>
2021-12-05 14:47:23 -0500

    add bank

--- financial_application/users/models.py
+++ financial_application/users/models.py
@@ -9,6 +9,10 @@ class Customer(models.Model):
     def __str__(self):
         return "Customer {}".format(self.last_name)
 
+class Bank(models.Model):
+    bank_name = models.CharField(max_length=100, default="bank")
+    routing_number = models.IntegerField()
+    bank_phone = models.CharField(max_length=100)
 
-
-
+    def __str__(self):
+        return "Bank {}".format(self.bank_name)
