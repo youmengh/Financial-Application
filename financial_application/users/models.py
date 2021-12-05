a0e566e4b411cf48a883d8a5f812645b547e658c
Youmeng <youmenghin15@gmail.com>
2021-12-05 14:51:14 -0500

    add customer_id

--- financial_application/users/models.py
+++ financial_application/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
     last_name = models.CharField(max_length=100, default="lastname")
     first_name = models.CharField(max_length=100, default="first")
     phone_number = models.CharField(max_length=100, default="phone")
@@ -16,8 +17,3 @@ class Bank(models.Model):
 
     def __str__(self):
         return "Bank {}".format(self.bank_name)
-
-
-
-
-
